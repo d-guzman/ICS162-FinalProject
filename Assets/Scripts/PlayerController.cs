2164b5cc75c4249faa52da2d699c8e450ab8e932
d-guzman <dghost121@gmail.com>
2019-03-15 18:29:22 -0700

    Player can Jump
    The player can now jump up and fall. The animations were set up as
    individual states in the animator, not as a blend tree. There is still
    some jank but the fact that they work at all is pretty good.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -28,17 +28,27 @@ public class PlayerController : MonoBehaviour
     private bool isJumping;
     private bool isFalling;
 
+    // Misc. Stuff.
+    private float distToGround;
+    private Bounds playerBounds;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
+        distToGround = GetComponent<CapsuleCollider>().bounds.extents.y;
+        playerBounds = GetComponent<CapsuleCollider>().bounds;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        checkGrounded();
+        checkFalling();
         Move();
+
+        if (!isJumping && isGrounded && Input.GetAxis("Jump") == 1)
+            Jump(); 
     }
 
     // Private Methods
@@ -58,5 +68,38 @@ public class PlayerController : MonoBehaviour
 
         anim.SetFloat("blendSpeed", movement.magnitude);
         rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+    }
+
+    private void Jump() {
+        isJumping = true;
+        isGrounded = false;
+        rb.velocity += Vector3.up * jumpStrength ;
+
+        anim.SetBool("animJump", true);
+        anim.SetBool("animFalling", false);
+    }
+
+    private void checkGrounded() {
+        if (Physics.Raycast(GetComponent<CapsuleCollider>().bounds.center, -Vector3.up, distToGround + 0.01f))
+        {
+            isGrounded = true;
+            isJumping = false;
+            isFalling = false;
+
+            anim.SetBool("animJump", false);
+            anim.SetBool("animFalling", false);
+        }
+        else
+            isGrounded = false; 
+    }
+    private void checkFalling() {
+        if (rb.velocity.y < -.01f)
+        {
+            isFalling = true;
+            isJumping = false;
+
+            anim.SetBool("animFalling", true);
+            anim.SetBool("animJump", false);
+        }
     }
 }
