18ad51a3e1a1d6e862ca347a59c448d07b8a1e26
David Jamgochian <jammer888@verizon.net>
2019-03-15 18:48:20 -0700

    Started creating SwitchInteractable interface and MovingPlatformController scripts. Also added a test scene for David

---
+++ Assets/Scripts/MovingPlatformController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformController : MonoBehaviour, SwitchInteractable
+{
+    [SerializeField]
+    private float moveDistance; // scalar distance of how far the platform should travel in total
+
+    [SerializeField]
+    private float speed;
+
+    public Vector3 direction; 
+    private Vector3 normalizedDirection;
+
+    private bool switchActivated;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        normalizedDirection = direction.normalized;
+        switchActivated = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnSwitchActivate() {
+        switchActivated = true;
+    }
+
+    public void OnSwitchDeactivate() {
+        switchActivated = false;
+    }
+}
---
+++ Assets/Scripts/SwitchInteractable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Have all interactables inherit from this interface.
+public interface SwitchInteractable 
+{
+    // defines what happens when activated by a switch.
+    void OnSwitchActivate();
+
+    // defines what happens when activated by a switch.
+    void OnSwitchDeactivate();
+}
