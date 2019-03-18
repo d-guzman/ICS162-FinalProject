4e3306b5ad05f547cbf253800e89de2a0bcc17ad
d-guzman <dghost121@gmail.com>
2019-03-18 01:34:58 -0700

    Initial punch animation
    Punch animation will play out; is rather jank because you can move
    during the followthrough but WHATEVER.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,63 +24,88 @@ public class PlayerController : MonoBehaviour
     //         this should only be true while the player is ASCENDING.
     //    - isFalling: Should be true whenever the player begins to fall
     //         after reaching the apex of their jump.
+    //    - isPunching: Should be true whenever the player presses the punch
+    //         button (either B on an Xbox Con. or LMB)
     private bool isGrounded;
     private bool isJumping;
     private bool isFalling;
+    private bool isPunching;
 
     // Misc. Stuff.
     private float distToGround;
-    private Bounds playerBounds;
+    private CapsuleCollider playerCollider;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         distToGround = GetComponent<CapsuleCollider>().bounds.extents.y;
-        playerBounds = GetComponent<CapsuleCollider>().bounds;
+        playerCollider = GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Debug.Log(anim.GetCurrentAnimatorClipInfo(0)[0].clip.name);
+
         checkGrounded();
         checkFalling();
         Move();
-
-        if (!isJumping && isGrounded && Input.GetAxis("Jump") == 1)
-            Jump(); 
+        Jump();
+        Punch();
     }
 
     // Private Methods
     private void Move() {
-        moveHori = Input.GetAxis("Horizontal");
-        moveVert = Input.GetAxis("Vertical");
-
-        movement = new Vector3(moveHori, 0f, moveVert);
-        if (movement.magnitude > 1f) { movement.Normalize(); }
-
-        if (moveHori != 0f || moveVert != 0f)
+        if (!isPunching)
         {
+            moveHori = Input.GetAxis("Horizontal");
+            moveVert = Input.GetAxis("Vertical");
+            movement = new Vector3(moveHori, 0f, moveVert);
 
-            Quaternion rotation = Quaternion.LookRotation(movement);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 10f * Time.deltaTime);
+            if (movement.magnitude > 1f) { movement.Normalize(); }
+
+            if (moveHori != 0f || moveVert != 0f)
+            {
+                Quaternion rotation = Quaternion.LookRotation(movement);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 10f * Time.deltaTime);
+            }
+
+            // This line of code makes the player slowly slide off platforms if they are on the edge of one.
+            if (isFalling) { movement += -(Vector3.up * .1f); }
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+
+            anim.SetFloat("blendSpeed", movement.magnitude);
         }
-
-        anim.SetFloat("blendSpeed", movement.magnitude);
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
     }
 
     private void Jump() {
-        isJumping = true;
-        isGrounded = false;
-        rb.velocity += Vector3.up * jumpStrength ;
+        if (!isJumping && isGrounded && Input.GetAxis("Jump") == 1)
+        {
+            isJumping = true;
+            isGrounded = false;
+            rb.velocity += Vector3.up * jumpStrength;
 
-        anim.SetBool("animJump", true);
-        anim.SetBool("animFalling", false);
+            anim.SetBool("animJump", true);
+            anim.SetBool("animFalling", false);
+        }
+    }
+
+    private void Punch() {
+        if (isGrounded && Input.GetAxis("Fire1") == 1)
+        {
+            isPunching = true;
+            anim.SetBool("animPunch", true);
+        }
+        if (isPunching && anim.GetCurrentAnimatorClipInfo(0)[0].clip.name == "Armature|Punch")
+        {
+            isPunching = false;
+            anim.SetBool("animPunch", false);
+        }
     }
 
     private void checkGrounded() {
-        if (Physics.Raycast(GetComponent<CapsuleCollider>().bounds.center, -Vector3.up, distToGround + 0.01f))
+        if (Physics.Raycast(playerCollider.bounds.center, -Vector3.up, distToGround + 0.01f))
         {
             isGrounded = true;
             isJumping = false;
