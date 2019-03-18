13701fcbb23b2b53733697cde50d745f3ed81f2b
David Jamgochian <jammer888@verizon.net>
2019-03-17 19:11:12 -0700

    Moving Platforms now roughly move correctly. PressurePlateSwitch script roughly works and started script for doors.

--- Assets/Scripts/MovingPlatformController.cs
+++ Assets/Scripts/MovingPlatformController.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class MovingPlatformController : MonoBehaviour, SwitchInteractable
 {
-    [SerializeField]
-    private float moveDistance; // scalar distance of how far the platform should travel in total
+    // [SerializeField]
+    // private float moveDistance; // scalar distance of how far the platform should travel in total
+
+    public Transform startPoint;
+    public Transform endPoint;
+
+    private float moveDistance;
 
     [SerializeField]
     private float speed;
@@ -15,21 +20,35 @@ public class MovingPlatformController : MonoBehaviour, SwitchInteractable
 
     private bool switchActivated;
 
+    private float startTime;
+    private Vector3 prevPosition;
+
     // Start is called before the first frame update
     void Start()
     {
         normalizedDirection = direction.normalized;
         switchActivated = false;
+        startTime = Time.time;
+        prevPosition = transform.position;
+        moveDistance = Vector3.Distance(startPoint.position, endPoint.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (switchActivated) {
+            float distance = speed * (Time.time - startTime) + Vector3.Distance(startPoint.position, prevPosition);
+            float frac = distance / moveDistance;
+            transform.position = Vector3.Lerp(startPoint.position, endPoint.position, frac);
+            // transform.Translate(normalizedDirection * speed * Time.deltaTime);
+        }
+        else {}
     }
 
     public void OnSwitchActivate() {
         switchActivated = true;
+        startTime = Time.time;
+        prevPosition = transform.position;
     }
 
     public void OnSwitchDeactivate() {
---
+++ Assets/Scripts/Switch Scripts/PressurePlateSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlateSwitch : MonoBehaviour
+{
+    public GameObject interactableObject;
+    private SwitchInteractable interactable;
+    private Collider trigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trigger = gameObject.GetComponent<Collider>();
+        interactable = interactableObject.GetComponent<SwitchInteractable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter() {
+        interactable.OnSwitchActivate();
+    }
+
+    private void OnTriggerExit() {
+        interactable.OnSwitchDeactivate();
+    }
+}
