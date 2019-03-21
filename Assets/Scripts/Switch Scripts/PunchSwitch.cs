60b8e18f9cab7ea84952d700515017a85d355d7b
David Jamgochian <jammer888@verizon.net>
2019-03-20 18:39:40 -0700

    Punch switch script now works, and punch switch prefab has been updated to inlcude and work with punch switch script.

--- Assets/Scripts/Switch Scripts/PunchSwitch.cs
+++ Assets/Scripts/Switch Scripts/PunchSwitch.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class PunchSwitch : MonoBehaviour
 {
+    [Tooltip("The game object that has a component that inherits from SwitchInteractable.")]
+    public GameObject interactableObject;
+    private SwitchInteractable interactable;
+    
+    [SerializeField]
+    private Collider collider; // may change later depending on actual model implementation!!!!
+
+    private bool switchPunched;
+
+    public Material[] materials;
+
+    private MeshRenderer renderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        switchPunched = false;
+        if (collider == null) {
+            collider = gameObject.GetComponent<Collider>();
+        }
+        interactable = interactableObject.GetComponent<SwitchInteractable>();
+
+        renderer = collider.gameObject.GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
@@ -15,4 +34,25 @@ public class PunchSwitch : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag.Equals("Hitbox")) {
+            Debug.Log("first instantiated material name: " + renderer.material.name);
+            Debug.Log("punch switch collided");
+            // punching switch activates or deactivates interactable depending on what state the switch is in.
+            // case where switch has not been punched
+            if (!switchPunched) {
+                renderer.material = materials[1];
+                interactable.OnSwitchActivate();
+            }
+            // case where switch has been punched
+            else {
+                renderer.material = materials[0];
+                interactable.OnSwitchDeactivate();
+            }
+
+            switchPunched = !switchPunched;    // swaps state
+        }
+    }
+
 }
